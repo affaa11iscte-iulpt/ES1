c524f99b80f570f5e0e8bb8da2c22707eb021062
Carol <Carol@172.17.10.113>
2017-12-15 14:46:15 +0000

    Revisão do nome das variáveis

--- src/Interface/Interface.java
+++ src/Interface/Interface.java
@@ -24,8 +24,7 @@ import javax.swing.table.DefaultTableModel;
 import components.Control;
 import components.Email;
 
-//pesquisar regras
-//rever as variaveis
+
 
 public class Interface extends JFrame {
 	protected DefaultTableModel tableModel;
@@ -33,29 +32,34 @@ public class Interface extends JFrame {
 	private List<Email> emails = new ArrayList<Email>();	
 	private JLabel fp = new JLabel("Falsos Positivos Gerados: ");
 	private JLabel fn = new JLabel("Falsos Negativos Gerados: ");
-	private JButton ok1 = new JButton("ok");
-	private JButton ok2 = new JButton("ok");
-	private JButton ok3 = new JButton("ok");
+	private JButton loadFile1 = new JButton("ok");
+	private JButton loadFile2 = new JButton("ok");
+	private JButton loadFile3 = new JButton("ok");
 	private Control c;
 	private Object[][] data;
 	private String[] colNomes={"Regras", "Pesos"};
 	private String fileRules;
-	private JCheckBox modoManual= new JCheckBox("Modo Manual");
-	private JCheckBox modoAutomatico= new JCheckBox("Modo Automatico");
+	private JCheckBox manualMode= new JCheckBox("Modo Manual");
+	private JCheckBox autoMode= new JCheckBox("Modo Automatico");
 
 
 	public Interface(){
 		c= new Control();
 		setSize(700,700);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		addComponentes();
+		addPanels();
 		setVisible(true);
 	}
 
 	public void open(){
 		setVisible(true);
 	}
-	public void addComponentes(){
+
+	/** 	
+	 * Funcao para juntar todos os paineis da interface
+	 **/
+
+	public void addPanels(){
 		setLayout(new BorderLayout());
 		add(filePanel(), BorderLayout.NORTH);
 		add(tablePanel(), BorderLayout.CENTER);
@@ -65,7 +69,7 @@ public class Interface extends JFrame {
 	}
 
 	/**
-	 * Criaï¿½ï¿½o do painel onde carrega os ficheiros escolhidos
+	 * Criacao do painel onde carrega os ficheiros escolhidos
 	 * return @JPanel
 	 */
 	private JPanel filePanel(){
@@ -73,56 +77,56 @@ public class Interface extends JFrame {
 		filePanel.setLayout(new GridLayout(3,2));
 
 		JPanel fileSubPanel1= new JPanel();
-		JLabel rules = new JLabel("rules.cf");
+		JLabel labelRules = new JLabel("rules.cf");
 		JTextField text1 = new JTextField("files\\rules.cf");
-		fileSubPanel1.add(rules);
+		fileSubPanel1.add(labelRules);
 		fileSubPanel1.add(text1);
-		fileSubPanel1.add(ok1);
-		ok1.addActionListener(new ActionListener(
+		fileSubPanel1.add(loadFile1);
+		loadFile1.addActionListener(new ActionListener(
 				) {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				fileRules = text1.getText();
 				addRules(fileRules);
-				ok1.setEnabled(false);
-				modoManual.setSelected(true);
-				modoAutomatico.setSelected(false);
+				loadFile1.setEnabled(false);
+				manualMode.setSelected(true);
+				autoMode.setSelected(false);
 
 			}
 		});
 
 		JPanel fileSubPanel2= new JPanel();
-		JLabel spam = new JLabel("spam.log");
+		JLabel labelSpam = new JLabel("spam.log");
 		JTextField text2 = new JTextField("files\\spam.log");
-		fileSubPanel2.add(spam);
+		fileSubPanel2.add(labelSpam);
 		fileSubPanel2.add(text2);
-		fileSubPanel2.add(ok2);
-		ok2.addActionListener(new ActionListener(
+		fileSubPanel2.add(loadFile2);
+		loadFile2.addActionListener(new ActionListener(
 				) {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				List<Email> spamEmails = Control.readEmails(text2.getText(), true);
 				emails.addAll(spamEmails);
-				ok2.setEnabled(false);
+				loadFile2.setEnabled(false);
 			}
 		});
 
 		JPanel fileSubPanel3= new JPanel();
-		JLabel ham = new JLabel("ham.log");
+		JLabel labelHam = new JLabel("ham.log");
 		JTextField text3 = new JTextField("files\\ham.log");
-		fileSubPanel3.add(ham);
+		fileSubPanel3.add(labelHam);
 		fileSubPanel3.add(text3);
-		fileSubPanel3.add(ok3);
-		ok3.addActionListener(new ActionListener(
+		fileSubPanel3.add(loadFile3);
+		loadFile3.addActionListener(new ActionListener(
 				) {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				List<Email> hamEmails = Control.readEmails(text3.getText(), false);
 				emails.addAll(hamEmails);
-				ok3.setEnabled(false);
+				loadFile3.setEnabled(false);
 			}
 		});
 
@@ -137,7 +141,7 @@ public class Interface extends JFrame {
 	}
 
 	/**
-	 * Criaï¿½ï¿½o do painel onde especifica a tabela e trata o modo, isto ï¿½, automï¿½tico e manual 
+	 * Criacao do painel onde especifica a tabela e trata o modo, isto e, automatico e manual 
 	 * return @JPanel
 	 */
 
@@ -163,49 +167,49 @@ public class Interface extends JFrame {
 		tablePanel.add(scrollArea);
 
 		JPanel tableSubPanel1 = new JPanel();
-		JLabel label = new JLabel("Calcular: ");
-		JButton calcular= new JButton("ok");
+		JLabel labelCalculate = new JLabel("Calcular: ");
+		JButton calculate= new JButton("ok");
 
-		tableSubPanel1.add(label);
-		tableSubPanel1.add(calcular);
+		tableSubPanel1.add(labelCalculate);
+		tableSubPanel1.add(calculate);
 
 		JPanel tableSubPanel2 = new JPanel();
 
-		modoManual.addActionListener(new ActionListener() {
+		manualMode.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				modoAutomatico.setSelected(false);
+				autoMode.setSelected(false);
 				isEditable=true;
 
 			}
 		});
 
-		modoAutomatico.addActionListener(new ActionListener() {
+		autoMode.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				isEditable=false;
-				modoManual.setSelected(false);
+				manualMode.setSelected(false);
 
 
 
 			}
 		});
 
-		calcular.addActionListener(new ActionListener() {
+		calculate.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if(ok1.isEnabled() || ok2.isEnabled() || ok3.isEnabled()){
+				if(loadFile1.isEnabled() || loadFile2.isEnabled() || loadFile3.isEnabled()){
 					JOptionPane.showMessageDialog(null, "Falta leitura de ficheiro");
 				}
 				else{
-					if(modoManual.isSelected()) {
+					if(manualMode.isSelected()) {
 						c = new Control();
 						c.calculate(emails,getRules() );
 						setFp(c.getFpos());
 						setFn(c.getFneg());
-					}else if(modoAutomatico.isSelected()) {
+					}else if(autoMode.isSelected()) {
 						c = new Control();
 						c.automaticMode(emails, getRules());
 						Map<String, String> rules = c.readAutomatic(getRules(), "experimentBaseDirectory\\referenceFronts\\AntiSpamFilterProblem.rf", 
@@ -220,8 +224,8 @@ public class Interface extends JFrame {
 			}
 		});
 
-		tableSubPanel2.add(modoManual);
-		tableSubPanel2.add(modoAutomatico);
+		tableSubPanel2.add(manualMode);
+		tableSubPanel2.add(autoMode);
 
 
 		tablePanel.add(scrollArea, BorderLayout.NORTH);
@@ -233,7 +237,7 @@ public class Interface extends JFrame {
 
 
 	/**
-	 * Criaï¿½ï¿½o do painel onde mostra os falsos positivos/negativos gerados e as opï¿½ï¿½es de guardar e apagar as configuraï¿½ï¿½es
+	 * Criacao do painel onde mostra os falsos positivos/negativos gerados e as opcoes de guardar e apagar as configuracoes
 	 * return @JPanel
 	 */
 	private JPanel modePanel(){
@@ -248,60 +252,51 @@ public class Interface extends JFrame {
 		modeSubPanel2.add(fn);
 
 		JPanel modeSubPanel3 = new JPanel();
-		JButton guardar = new JButton ("Guardar Configuraï¿½ï¿½o");
-		modeSubPanel3.add(guardar);
-		
-		guardar.addActionListener(new ActionListener() {
-			
+		JButton save = new JButton ("Guardar Configuração");
+		modeSubPanel3.add(save);
+
+		save.addActionListener(new ActionListener() {
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				c.saveConfigurations(fileRules, getRules());
-				
+
 			}
 		});
 
 		JPanel modeSubPanel4 = new JPanel();
-		JButton reset = new JButton ("Reset Configuraï¿½ï¿½o");
+		JButton reset = new JButton ("Reset Configuração");
 		reset.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				ok1.setEnabled(true);
-				ok2.setEnabled(true);
-				ok3.setEnabled(true);
+				loadFile1.setEnabled(true);
+				loadFile2.setEnabled(true);
+				loadFile3.setEnabled(true);
 				setFp(0);
 				setFn(0);
-				if(modoAutomatico.isSelected() || modoManual.isSelected()){
-					modoAutomatico.setSelected(false);
-					modoManual.setSelected(false);
+				if(autoMode.isSelected() || manualMode.isSelected()){
+					autoMode.setSelected(false);
+					manualMode.setSelected(false);
 				}
-			removeAllRowsOfTable();
-			c.removeRowFile();
-			
+				removeAllRowsOfTable();
+				c.removeRowFile();
+
 
 			}
 		});
 
 		modeSubPanel4.add(reset);
-
-
 		modePanel.add(modeSubPanel1);
 		modePanel.add(modeSubPanel2);
 		modePanel.add(modeSubPanel3);
 		modePanel.add(modeSubPanel4);
 
-
 		return modePanel;
 	}
 
-	
-
-		
-	
-
-
 	/**
-	 * Altera o valor das labels dos falsos positivos quando este ï¿½ calculado, recebe um inteiro (numero de falsos positivos), que vai ser o valor a adicionar na label 
+	 * Altera o valor das labels dos falsos positivos quando este e calculado, recebe um inteiro (numero de falsos positivos), que vai ser o valor a adicionar na label 
 	 * @param i
 	 */
 	public void setFp(int i) {
@@ -309,7 +304,7 @@ public class Interface extends JFrame {
 	}
 
 	/**
-	 * Altera o valor das labels dos falsos negativos quando este ï¿½ calculado, recebe um inteiro (numero de falsos negativos), que vai ser o valor a adicionar na label 
+	 * Altera o valor das labels dos falsos negativos quando este e calculado, recebe um inteiro (numero de falsos negativos), que vai ser o valor a adicionar na label 
 	 * @param i
 	 */
 	public void setFn(int i) {
@@ -318,13 +313,13 @@ public class Interface extends JFrame {
 
 
 	/**
-	 * Obtenï¿½ï¿½o do HashMap de regras (com pesos associados) lidas na classe Reader
+	 * Obtencao do HashMap de regras (com pesos associados) lidas na classe Reader
 	 */
 	public void addRules(String file) {
 		Map<String,String> rules = Control.readRules(file);
 		putRulesOnTable(rules);
 	}
-	
+
 	/**
 	 * Listagem das regras na tabela da interface
 	 * @param rules Mapa de regras <Regra,Peso>
@@ -335,26 +330,30 @@ public class Interface extends JFrame {
 			tableModel.addRow(new String[] {entry.getKey(), entry.getValue()});
 		}
 	}
-	
+
+	/** 
+	 *Reescreve a tabela 
+	 *
+	 **/
 	private void cleanTable(Map<String, String> rules) {
 		removeAllRowsOfTable();
 		for(Map.Entry<String, String> entry: rules.entrySet()) {
 			tableModel.addRow(new String[] {entry.getKey(), ""});
 		}
 	}
-	
+
 	/**
 	 * Remove todas as linhas da tabela
 	 */
 	private void removeAllRowsOfTable() {
 		if(tableModel.getRowCount() > 0)
-		for (int i = tableModel.getRowCount() - 1; i > -1; i--) {
-	        tableModel.removeRow(i);
-	    }
+			for (int i = tableModel.getRowCount() - 1; i > -1; i--) {
+				tableModel.removeRow(i);
+			}
 	}
 
 	/**
-	 * Devolve as regras e os respetivos pesos num HashMap, sendo que a chave ï¿½ a regra
+	 * Devolve as regras e os respetivos pesos num HashMap, sendo que a chave e a regra
 	 * e o valor o peso.
 	 * @return Lista de regras com os pesos
 	 */

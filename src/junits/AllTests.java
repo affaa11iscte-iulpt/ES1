172e777b70e83eafd34eeec28651cdef6b185f66
Rafael Santos <jrcss@iscte-iul.pt>
2017-12-16 15:53:52 +0000

    JUnit de Control

---
+++ src/junits/AllTests.java
@@ -0,0 +1,15 @@
+package junits;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	ControlTest.class, 
+	EmailTest.class 
+})
+
+public class AllTests {
+	
+}
